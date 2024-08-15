4ef10e86de84265eddf3ae4890600d9b6ba4994b
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-08-14 23:29:00 -0400

    photo upload working, converting back to png with sharp

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const multer = require("multer");
-const upload = multer({ dest: "public/uploads/" });
+const sharp = require("sharp");
+const fs = require("fs");
+const path = require("path");
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -20,10 +25,23 @@ app.post("/urlencoded-form", (req, res) => {
   );
 });
 
-app.post("/multipart-form", upload.single("photo"), (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
-  res.send("Thanks for your photo!");
+app.post("/multipart-form", upload.single("photo"), async (req, res) => {
+  if (!req.file) {
+    res.status(400).send("You have to include a photo!");
+  }
+  const fileName = req.file.originalname;
+  const filepath = path.join(__dirname, "public/uploads/", fileName);
+
+  try {
+    await sharp(req.file.buffer).png().toFile(filepath);
+    console.log("photo saved!");
+    res.send(
+      `Thanks for uploading ${req.file.originalname}, we'll keep it safe :)`
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("something went wrong on our end!");
+  }
 });
 
 app.listen(3500, () => {
