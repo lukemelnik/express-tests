6837a6bc4194d7bbdd41a6912838984e223944db
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-08-15 09:34:47 -0400

    add sessions and protected routes

--- server-2.js
+++ server-2.js
@@ -4,45 +4,86 @@ const sharp = require("sharp");
 const fs = require("fs");
 const path = require("path");
 const busboy = require("busboy");
+const session = require("express-session");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 300000, secure: false },
+    name: "sessionId",
+  })
+);
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (username === "admin" && password === "password") {
+    req.session.user = username;
+    req.session.save((err) => {
+      if (err) {
+        console.log("session save error");
+        return res.status(500).send("Session save error");
+      } else {
+        res.redirect("/multipart-form");
+      }
+    });
+  } else {
+    res.status(401).send("Invalid username or password");
+  }
+});
+
+app.get("/multipart-form", (req, res) => {
+  console.log(req.session);
+  if (!req.session.user) {
+    return res.status(301).redirect("/login");
+  } else {
+    return res.sendFile(path.join(__dirname, "public", "multipart-form.html"));
+  }
+});
 
 app.post("/multipart-form", async (req, res) => {
-  // create a new busboy instance
+  console.log(req.session);
+  if (req.session.user !== "admin") {
+    return res.status(301).redirect("/login");
+  }
+
   let bb = busboy({ headers: req.headers });
 
-  // define the busboy event listeners
   bb.on("file", (name, file, info) => {
     const { filename, encoding, mimeType } = info;
-    // create a writable stream to save the file to disk
     const writeableStream = fs.createWriteStream(
       path.join(__dirname, "public/uploads/", filename)
     );
-    // create a transformer that resizes the image using sharp
     const transformer = sharp()
       .png()
       .resize(200)
       .on("info", ({ height }) => console.log(`Image height is ${height}`));
 
-    // pipe the file stream through the transformer and then to the writable stream
     file
       .pipe(transformer)
       .pipe(writeableStream)
       .on("finish", () => {
-        res.send(`Thanks for uploading ${filename}, we'll keep it safe :)`);
+        return res.send(
+          `Thanks for uploading ${filename}, we'll keep it safe :)`
+        );
       })
       .on("error", (error) => {
         console.log(error);
-        res.status(500).send("something went wrong on our end!");
+        return res.status(500).send("Something went wrong on our end!");
       });
   });
-  // seprarate error handling for other parts of the busboy process
+
   bb.on("error", (error) => {
     console.log(error);
-    res.status(500).send("something went wrong on our end!");
+    return res.status(500).send("Something went wrong on our end!");
   });
-  // we defined event listeners but didn't actually pipe the request to busboy yet, do that here:
+
   req.pipe(bb);
 });
 
