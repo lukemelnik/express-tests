6df987716847b6a6e351501a10c3be041b37e44b
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-08-14 23:02:03 -0400

    post request working, reading headers and body

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/urlencoded-form", (req, res) => {
   console.log(req.query);
-  res.end();
+  res.redirect("/");
 });
 
 app.post("/urlencoded-form", (req, res) => {
-  console.log(req.headers);
+  console.log(req.headers.origin);
   console.log(req.body);
-  res.end();
+  res.send(
+    `<html><h1>Thanks for signing up ${req.body.name}!</h1>
+    <a href="/">Head back to the form page -></a></html>`
+  );
 });
 
 app.listen(3500, () => {
