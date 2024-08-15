3e2824d435f0898f876e9c8d98dff83df8d607c8
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-08-14 23:15:09 -0400

    install multer, upload file working

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const multer = require("multer");
+const upload = multer({ dest: "public/uploads/" });
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -16,6 +18,12 @@ app.post("/urlencoded-form", (req, res) => {
     `<html><h1>Thanks for signing up ${req.body.name}!</h1>
     <a href="/">Head back to the form page -></a></html>`
   );
+});
+
+app.post("/multipart-form", upload.single("photo"), (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  res.send("Thanks for your photo!");
 });
 
 app.listen(3500, () => {
